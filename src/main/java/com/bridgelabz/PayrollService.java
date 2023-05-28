beb58abddf58e4446ea7296205e0a55a6a9d5106
Yash <imyashpawar@gmail.com>
2023-05-28 17:14:32 +0530

    Added: Read employee fro console

--- src/main/java/com/bridgelabz/PayrollService.java
+++ src/main/java/com/bridgelabz/PayrollService.java
@@ -1,8 +1,27 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class PayrollService {
 
-    public static void main(String[] args) {
-        System.out.println("Payroll Service");
+    Employee employee =new Employee();
+    Scanner sc = new Scanner(System.in);
+
+    public void employee(){
+        System.out.println("Enter a Employee Name: ");
+        String name =sc.next();
+
+        System.out.println("Enter a Salary ");
+        double salary= sc.nextDouble();
+
+        employee.setName(name);
+        employee.setSalary(salary);
     }
+
+
+
+
+
+
+
 }
---
+++ src/main/java/com/bridgelabz/PayrollServiceMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class PayrollServiceMain {
+    public static void main(String[] args) {
+
+        PayrollService payrollService = new PayrollService();
+        payrollService.employee();
+
+    }
+}
